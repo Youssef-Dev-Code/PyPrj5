18c7fd5fd9da50e125059586518d14d6d612a552
Youssef-Dev-Code <youssefazaiez21@gmail.com>
2023-08-15 13:23:11 +0100

    com

--- website/models.py
+++ website/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 
 class Employee(models.Model):
-    def __init__(self):
-        First = models.CharField(max_length=20)
-        Last =  models.CharField(max_length=20)
-        Email = models.EmailField(max_length=60)
-        CIN = models.CharField(max_length=8)
-        Date_Naissance = models.DateField()
-        Salary = models.DecimalField(max_digits=12, decimal_places=3)
+    First = models.CharField(max_length=20  , default= "Something")
+    Last =  models.CharField(max_length=20  , default= "Something")
+    Email = models.EmailField(max_length=60 , default= "Something")
+    Salary = models.DecimalField(max_digits=12, decimal_places=3, default= 0)
--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import response, request
 from django.contrib import messages
-
+from .models import *
 
 def index(request):
     context = dict() 
