5227db42d4aabe11d39557e8be53f1f8d0212625
Youssef-Dev-Code <youssefazaiez21@gmail.com>
2023-08-15 13:08:09 +0100

    commit

--- website/models.py
+++ website/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Employee(models.Model):
-    def __init__(self,First: models.CharField(max_length=20),Last: models.CharField(max_length=20),Email: models.EmailField(max_length=60),CIN: models.CharField(max_length=8),Date_Naissance : models.DateField(),Salary: models.DecimalField()):
-        self.First = First
-        self.Last = Last
-        self.Email = Email
-        self.CIN = CIN
-        self.Date_Naissance = Date_Naissance
-        self.Salary = Salary
+    def __init__(self):
+        First = models.CharField(max_length=20)
+        Last =  models.CharField(max_length=20)
+        Email = models.EmailField(max_length=60)
+        CIN = models.CharField(max_length=8)
+        Date_Naissance = models.DateField()
+        Salary = models.DecimalField(max_digits=12, decimal_places=3)
--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import response, request
 from django.contrib import messages
-from .models import *
+
 
 def index(request):
     context = dict() 
-    return render(request=request, template_name= Employee, context= context)
+    return render(request=request, template_name= "index.html", context= context)
